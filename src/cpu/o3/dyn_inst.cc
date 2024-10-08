7889971d3c88c3edcd536d25625220d2ca2a3877
siliazerva <125261582+siliazerva@users.noreply.github.com>
2024-10-08 13:29:09 +0300

    Update dyn_inst.cc

--- src/cpu/o3/dyn_inst.cc
+++ src/cpu/o3/dyn_inst.cc
@@ -59,7 +59,7 @@ DynInst::DynInst(const Arrays &arrays, const StaticInstPtr &static_inst,
       _numSrcs(arrays.numSrcs), _numDests(arrays.numDests),
       _flatDestIdx(arrays.flatDestIdx), _destIdx(arrays.destIdx),
       _prevDestIdx(arrays.prevDestIdx), _srcIdx(arrays.srcIdx),
-      _readySrcIdx(arrays.readySrcIdx), macroop(_macroop),cluster_id(0)
+      _readySrcIdx(arrays.readySrcIdx), macroop(_macroop),cluster_id(0),needsClusterDelay(false)
 {
     std::fill(_readySrcIdx, _readySrcIdx + (numSrcs() + 7) / 8, 0);
 
