e39a2a198d9818b2809a7b380163a35106dc32a6
siliazerva <125261582+siliazerva@users.noreply.github.com>
2024-11-11 14:28:14 +0200

    Update BaseO3CPU.py

--- src/cpu/o3/BaseO3CPU.py
+++ src/cpu/o3/BaseO3CPU.py
@@ -188,6 +188,6 @@ class BaseO3CPU(BaseCPU):
     smtCommitPolicy = Param.CommitPolicy("RoundRobin", "SMT Commit Policy")
 
     branchPred = Param.BranchPredictor(
-        TournamentBP(numThreads=Parent.numThreads), "Branch Predictor"
+        TAGE_SC_L_TAGE_64KB(numThreads=Parent.numThreads), "Branch Predictor"
     )
     needsTSO = Param.Bool(False, "Enable TSO Memory model")
