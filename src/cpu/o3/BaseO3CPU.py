de2759398788586ed2e4f2df7cb5a6bec13fbd6d
siliazerva <125261582+siliazerva@users.noreply.github.com>
2024-11-15 17:35:35 +0200

    Update BaseO3CPU.py

--- src/cpu/o3/BaseO3CPU.py
+++ src/cpu/o3/BaseO3CPU.py
@@ -137,8 +137,8 @@ class BaseO3CPU(BaseCPU):
         5, "Time buffer size for forward communication"
     )
 
-    LQEntries = Param.Unsigned(24, "Number of load queue entries")
-    SQEntries = Param.Unsigned(24, "Number of store queue entries")
+    LQEntries = Param.Unsigned(192, "Number of load queue entries")
+    SQEntries = Param.Unsigned(114, "Number of store queue entries")
     LSQDepCheckShift = Param.Unsigned(
         4, "Number of places to shift addr before check"
     )
@@ -158,10 +158,10 @@ class BaseO3CPU(BaseCPU):
     numRobs = Param.Unsigned(1, "Number of Reorder Buffers")
 
     numPhysIntRegs = Param.Unsigned(
-        64, "Number of physical integer registers"
+        280, "Number of physical integer registers"
     )
     numPhysFloatRegs = Param.Unsigned(
-        64, "Number of physical floating point registers"
+        332, "Number of physical floating point registers"
     )
     numPhysVecRegs = Param.Unsigned(256, "Number of physical vector registers")
     numPhysVecPredRegs = Param.Unsigned(
@@ -170,8 +170,8 @@ class BaseO3CPU(BaseCPU):
     numPhysMatRegs = Param.Unsigned(2, "Number of physical matrix registers")
     # most ISAs don't use condition-code regs, so default is 0
     numPhysCCRegs = Param.Unsigned(0, "Number of physical cc registers")
-    numIQEntries = Param.Unsigned(24, "Number of instruction queue entries")
-    numROBEntries = Param.Unsigned(32, "Number of reorder buffer entries")
+    numIQEntries = Param.Unsigned(97, "Number of instruction queue entries")
+    numROBEntries = Param.Unsigned(512, "Number of reorder buffer entries")
 
     smtNumFetchingThreads = Param.Unsigned(1, "SMT Number of Fetching Threads")
     smtFetchPolicy = Param.SMTFetchPolicy("RoundRobin", "SMT Fetch policy")
