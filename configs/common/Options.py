488967b9332b3b3df4a14c360c0cd01acc850625
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-01-10 17:13:46 +0200

    Update Options.py

--- configs/common/Options.py
+++ configs/common/Options.py
@@ -688,6 +688,13 @@ def addCommonOptions(parser, default_isa: Optional[ISA] = None):
         default=None,
         help="Override vendor string returned by CPUID instruction in X86.",
     )
+    parser.add_argument(
+    "--num-clusters",
+    type=int,
+    default=2,
+    help="Number of clusters."
+)
+
 
 
 def addSEOptions(parser):
