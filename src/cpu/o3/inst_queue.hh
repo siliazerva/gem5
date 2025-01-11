c52c725ee4693790d1cf04b3c6682f48e2d347fb
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-01-11 17:59:44 +0200

    Update inst_queue.hh

--- src/cpu/o3/inst_queue.hh
+++ src/cpu/o3/inst_queue.hh
@@ -298,7 +298,7 @@ class InstructionQueue
 
     /** Pointer to IEW stage. */
     IEW *iewStage;
-
+    int num_clusters;
     /** The memory dependence unit, which tracks/predicts memory dependences
      *  between instructions.
      */
