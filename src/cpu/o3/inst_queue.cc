6a1ac71592a4a2171ed6b9e5cfbd4698c26dcae8
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-01-11 19:15:32 +0200

    Update inst_queue.cc

--- src/cpu/o3/inst_queue.cc
+++ src/cpu/o3/inst_queue.cc
@@ -740,7 +740,7 @@ InstructionQueue::processFUCompletion(const DynInstPtr &inst, int fu_idx)
    --wbOutstanding;
     iewStage->wakeCPU();
     //FUPool *fuPool = (inst->cluster_id == 0) ? fuPool1 : fuPool2;
-FUPool *fuPool = (inst->cluster_id < fuPools.size()) ? &fuPools[inst->cluster_id] : nullptr;
+FUPool *fuPool = (inst->cluster_id < fuPools.size()) ? fuPools[inst->cluster_id] : nullptr;
 
 
     if (fu_idx > -1)
@@ -822,8 +822,8 @@ InstructionQueue::scheduleReadyInsts()
         }
         //FUPool *fuPool = (issuing_inst->cluster_id == 0) ? fuPool1 : fuPool2;
         //FUPool *fuPool = nullptr;
-	FUPool *fuPool = (issuing_inst->cluster_id < fuPools.size()) ? &fuPools[issuing_inst->cluster_id] : nullptr;
-
+	FUPool *fuPool = (issuing_inst->cluster_id < fuPools.size()) ? fuPools[issuing_inst->cluster_id] : nullptr;
+	
 
         //fuPool = (issuing_inst->cluster_id == 0) ? fuPool1 : fuPool2;
         int idx = FUPool::NoCapableFU;
@@ -852,7 +852,7 @@ int selectedCluster = -1;
 // Loop through all available FUPools (based on num_clusters) to find the one with the least load
 for (int i = 0; i < fuPools.size(); ++i) {
     // Get the load for the current cluster
-    float load = fuPools[i].getRelativeLoad(); // Access the correct FUPool dynamically
+    float load = fuPools[i]->getRelativeLoad(); // Access the correct FUPool dynamically
 
     // Update the selected cluster if this one has less load
     if (load < minLoad) {
