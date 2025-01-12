af4a650ba16ad92d888d8ea2f5af74c832509548
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-01-12 15:59:26 +0200

    Update BaseO3CPU.py

--- src/cpu/o3/BaseO3CPU.py
+++ src/cpu/o3/BaseO3CPU.py
@@ -127,10 +127,11 @@ class BaseO3CPU(BaseCPU):
     num_clusters = Param.Int(2, "Number of clusters in the CPU")
     def __init__(self, *args, **kwargs):
         super(BaseO3CPU, self).__init__(*args, **kwargs)
+        print(f"BaseO3CPU initialized with {self.num_clusters} clusters.")
         for i in range(self.num_clusters):
             new_fupool = DefaultFUPool()  # Or FUPool() if DefaultFUPool isn't appropriate
             self.fuPools.append(new_fupool)
-
+            print(f"FU pool {i} added.")
 
     iewToCommitDelay = Param.Cycles(
         1, "Issue/Execute/Writeback to commit delay"
