ae65cdc1e5364a86032ef94f6233d986bd258ef4
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-01-15 22:51:58 +0200

    Update FuncUnitConfig.py

--- src/cpu/o3/FuncUnitConfig.py
+++ src/cpu/o3/FuncUnitConfig.py
@@ -37,6 +37,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from m5.defines import buildEnv
+from m5.objects.FUPool import *
 from m5.objects.FuncUnit import *
 from m5.params import *
 from m5.SimObject import SimObject
