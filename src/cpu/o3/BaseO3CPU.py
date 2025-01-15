0a7710d1f3333cfa79542934190003b9c4423732
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-01-15 12:15:44 +0200

    Update BaseO3CPU.py

--- src/cpu/o3/BaseO3CPU.py
+++ src/cpu/o3/BaseO3CPU.py
@@ -41,6 +41,7 @@ from m5.objects.BaseCPU import BaseCPU
 
 # from m5.objects.O3Checker import O3Checker
 from m5.objects.BranchPredictor import *
+from m5.objects.FuncUnitConfig import *
 from m5.objects.FUPool import *
 from m5.params import *
 from m5.proxy import *
