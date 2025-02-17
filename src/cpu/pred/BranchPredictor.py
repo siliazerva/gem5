1da7109cc85c9ae9b36d618334d58c68b937c690
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-02-17 10:51:23 +0200

    Update BranchPredictor.py

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -89,7 +89,7 @@ class SimpleBTB(BranchTargetBuffer):
     cxx_header = "cpu/pred/simple_btb.hh"
 
     numEntries = Param.Unsigned(4096, "Number of BTB entries")
-    tagBits = Param.Unsigned(2, "Size of the BTB tags, in bits")
+    tagBits = Param.Unsigned(16, "Size of the BTB tags, in bits")
     instShiftAmt = Param.Unsigned(
         Parent.instShiftAmt, "Number of bits to shift instructions by"
     )
