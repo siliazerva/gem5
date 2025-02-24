6a875cc90b9796630a027b86c5e07af953ddba5f
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-02-24 13:28:19 +0200

    Update se.py

--- configs/deprecated/example/se.py
+++ configs/deprecated/example/se.py
@@ -186,7 +186,7 @@ if args.smt and args.num_cpus > 1:
 np = args.num_cpus
 mp0_path = multiprocesses[0].executable
 system = System(
-    cpu=[CPUClass(cpu_id=i) for i in range(np)],
+    cpu=[CPUClass(cpu_id=i, width=args.width) for i in range(np)],
     mem_mode=test_mem_mode,
     mem_ranges=[AddrRange(args.mem_size)],
     cache_line_size=args.cacheline_size,
