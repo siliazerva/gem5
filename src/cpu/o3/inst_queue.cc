c8e87c0dd24b8ac072290b2f73110c41031ba287
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-03-09 14:14:11 +0200

    Update inst_queue.cc

--- src/cpu/o3/inst_queue.cc
+++ src/cpu/o3/inst_queue.cc
@@ -1212,7 +1212,7 @@ InstructionQueue::wakeDependents(const DynInstPtr &completed_inst)
         //Go through the dependency chain, marking the registers as
         //ready within the waiting instructions.
         DynInstPtr dep_inst = dependGraph.pop(dest_reg->flatIndex());
-        Cycles extraDelay = Cycles(1);
+        Cycles extraDelay = Cycles(2);
          while (dep_inst) {
 	    totalDependents++;
             DPRINTF(IQ, "Waking up a dependent instruction, [sn:%llu] "
