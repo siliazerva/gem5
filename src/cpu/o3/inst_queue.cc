ecb967d78731f7acea44abae669283db64a29065
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-03-11 13:11:09 +0200

    Update inst_queue.cc

--- src/cpu/o3/inst_queue.cc
+++ src/cpu/o3/inst_queue.cc
@@ -1228,7 +1228,7 @@ InstructionQueue::wakeDependents(const DynInstPtr &completed_inst)
                
                 dep_inst->needsClusterDelay = true; 
 		interClusterDependents++;
-		MemDepEntryPtr entry = memDepUnit[tid].findInHash(dep_inst);
+		MemDepEntryPtr entry = memDepUnit[tid].getMemDepEntryPtr(dep_inst);
 
 		entry->pendingEvents++;
             }
@@ -1247,7 +1247,7 @@ if (dep_inst->needsClusterDelay && !dep_inst->isEventScheduled()) {
 	DPRINTF(IQ, "Percentage of delayed instructions is %.2f%%\n", percentage); 
         dep_inst->needsClusterDelay = false;
         dep_inst->setEventScheduled(false);
-	MemDepEntryPtr entry = memDepUnit[tid].findInHash(dep_inst);
+	MemDepEntryPtr entry = memDepUnit[tid].getMemDepEntryPtr(dep_inst);
     	entry->pendingEvents--;
 }, "ClusterDelayEvent", true), cpu->clockEdge(extraDelay));
 
