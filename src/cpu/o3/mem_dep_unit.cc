b33b4317817eee05db0eecc2a1ab5b7903445072
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-03-11 14:43:50 +0200

    Update mem_dep_unit.cc

--- src/cpu/o3/mem_dep_unit.cc
+++ src/cpu/o3/mem_dep_unit.cc
@@ -414,11 +414,11 @@ MemDepUnit::completed(const DynInstPtr &inst)
 
     assert(hash_it != memDepHash.end());
 
-    if ((*hash_it).second->pendingEvents > 0) {
+    /*if ((*hash_it).second->pendingEvents > 0) {
         DPRINTF(MemDepUnit, "Not removing inst with pending events [sn:%lli].\n",
                 inst->seqNum);
         return;  // Don't remove 
-    }
+    }*/
 
     instList[tid].erase((*hash_it).second->listIt);
 
