c8f33856c0e44ed1d732b1cceadf60e0d1a1526d
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-03-11 21:07:29 +0200

    Update mem_dep_unit.cc

--- src/cpu/o3/mem_dep_unit.cc
+++ src/cpu/o3/mem_dep_unit.cc
@@ -138,6 +138,18 @@ MemDepUnit::decrementPendingEvents(const DynInstPtr &inst)
         }
     }
 }
+bool
+MemDepUnit::hasPendingEvents(const DynInstPtr &inst)
+{
+    auto it = memDepHash.find(inst);
+    if (it != memDepHash.end()) {
+        MemDepEntryPtr entry = it->second;
+        if (entry) {
+            return entry->pendingEvents > 0;
+        }
+    }
+    return false;
+}
 
 bool
 MemDepUnit::isDrained() const
