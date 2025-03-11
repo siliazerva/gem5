0cb7e881adcc0e442315798abb9ee6c59f0042b5
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-03-11 14:03:09 +0200

    Update mem_dep_unit.hh

--- src/cpu/o3/mem_dep_unit.hh
+++ src/cpu/o3/mem_dep_unit.hh
@@ -160,10 +160,7 @@ class MemDepUnit
 
     /** Debugging function to dump the lists of instructions. */
     void dumpLists();
-    std::shared_ptr<MemDepEntry> getMemDepEntry(const DynInstConstPtr& inst) {
-        // Access private findInHash function
-        return std::shared_ptr<MemDepEntry>(&findInHash(inst));  
-    }
+
   private:
     /** Completes a memory instruction. */
     void completed(const DynInstPtr &inst);
