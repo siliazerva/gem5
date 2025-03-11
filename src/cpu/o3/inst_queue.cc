b29f38368299621546c7acab9bdc458586a57f70
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-03-11 12:25:19 +0200

    Update inst_queue.cc

--- src/cpu/o3/inst_queue.cc
+++ src/cpu/o3/inst_queue.cc
@@ -52,6 +52,7 @@
 #include "enums/OpClass.hh"
 #include "params/BaseO3CPU.hh"
 #include "sim/core.hh"
+#include "cpu/o3/mem_dep_unit.hh"
 
 // clang complains about std::set being overloaded with Packet::set if
 // we open up the entire namespace std
