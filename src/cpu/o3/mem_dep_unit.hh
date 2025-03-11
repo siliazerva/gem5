6813144675f0b9cde46f55380d50f3115d4666bb
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-03-11 15:11:32 +0200

    Update mem_dep_unit.hh

--- src/cpu/o3/mem_dep_unit.hh
+++ src/cpu/o3/mem_dep_unit.hh
@@ -53,7 +53,7 @@
 #include "cpu/o3/limits.hh"
 #include "cpu/o3/store_set.hh"
 #include "debug/MemDepUnit.hh"
-
+#include "cpu/o3/inst_queue.hh"
 namespace gem5
 {
 
@@ -162,6 +162,7 @@ class MemDepUnit
     void dumpLists();
   private:
     /** Completes a memory instruction. */
+    friend class InstructionQueue;
     void completed(const DynInstPtr &inst);
 
     /** Wakes any dependents of a memory instruction. */
