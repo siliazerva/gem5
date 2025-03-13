81851673b1f6554af911d54c7621fb5ef1182dba
siliazerva <125261582+siliazerva@users.noreply.github.com>
2025-03-13 02:02:40 +0200

    Update inst_queue.cc

--- src/cpu/o3/inst_queue.cc
+++ src/cpu/o3/inst_queue.cc
@@ -1221,7 +1221,7 @@ InstructionQueue::wakeDependents(const DynInstPtr &completed_inst)
     		DPRINTF(IQ, "Scheduling delay for instruction [sn:%llu]\n", dep_inst->seqNum);
 		DynInstPtr completedInstPtr = completed_inst;
     		cpu->schedule(new EventFunctionWrapper([this, dep_inst, completedInstPtr, tid]() {
-        		if (completedInstPtr->isSquashed()) return;
+        		if (completedInstPtr->isSquashed() || dep_inst->isSquashed()) return;
 			dep_inst->markSrcRegReady();
         		addIfReady(dep_inst);
         		DPRINTF(IQ, "Instruction [sn:%llu] is marked ready after delay.\n", dep_inst->seqNum); 
